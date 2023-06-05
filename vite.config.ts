8b4c0459555d2ac9f78d2e83de1678ed78f6389f
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-06-05 23:09:12 +0300

    refactor: use dts-bundle-gen over vite-dts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +11,7 @@ export default defineConfig({
       "~": resolve(__dirname, "./lib")
     }
   },
-  plugins: [react(), dts()],
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
