27471ffc506225679024eba5384604e5794539c1
Mikael Siidorow <mikael.siidorow@aalto.fi>
2023-07-24 21:05:48 +0300

    feat: add <Sheet /> component

--- lib/main.ts
+++ lib/main.ts
@@ -2,4 +2,5 @@ export * from "~/components/Button";
 export * from "~/components/Collapsible";
 export * from "~/components/ScrollArea";
 export * from "~/components/Separator";
+export * from "~/components/Sheet";
 export * from "~/components/Tabs";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -86,5 +86,5 @@ module.exports = {
       })
     }
   },
-  plugins: []
+  plugins: [require("tailwindcss-animate")]
 };
